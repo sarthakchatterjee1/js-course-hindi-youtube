60ff31d3e06fbe625eee726a6c550882347f1442
SARTHAK CHATTERJEE <67676840+sarthakchatterjee1@users.noreply.github.com>
2024-02-22 22:20:26 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "sarthak";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueNumber = Number(score)
+// console.log(valueNumber)
+// console.log(typeof valueNumber)
+
+/*
+"33" => 33
+"33abc" => NaN => Not a Number (full form)
+true => 1 ; false => 0 
+*/
+let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Sarthak" => true
+
+let stringNumber = String(isLoggedIn)
+console.log(stringNumber)
+console.log(typeof stringNumber)
