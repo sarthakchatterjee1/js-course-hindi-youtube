a960ff6f459ff4652926c530df32bf728d432be8
SARTHAK CHATTERJEE <67676840+sarthakchatterjee1@users.noreply.github.com>
2024-02-23 23:23:02 +0000

    String

---
+++ 01_basics/05_string.js
@@ -0,0 +1,5 @@
+const name = "Sarthak"
+const repoCount = 50;
+// console.log(name + repoCount);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+//console.log('Hello my name is ${name} and my repo count is ${repoCount}');
