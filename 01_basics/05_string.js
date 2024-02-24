ef6150ec3ca874b24fbc617058ec2dc4082d5e0f
SARTHAK CHATTERJEE <67676840+sarthakchatterjee1@users.noreply.github.com>
2024-02-24 15:40:12 +0000

    sring concept

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,22 +1,27 @@
 const name = "Sarthak"
 const repoCount = 50;
 // console.log(name + repoCount);
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 //console.log('Hello my name is ${name} and my repo count is ${repoCount}');
 
 
-  const gameName = new String('Sar-thak')
+  const gameName = new String('Sarthak')
 
-  console.log(gameName[0]);
-  console.log(gameName.__proto__);
+//   console.log(gameName[0]);
+//   console.log(gameName.__proto__);
 
-  console.log(gameName.length);
-  console.log(gameName.toUpperCase());
-  console.log(gameName.charAt(3));
-  console.log(gameName.indexOf('t'));
+//   console.log(gameName.length);
+//   console.log(gameName.toUpperCase());
+//   console.log(gameName.charAt(3));
+//   console.log(gameName.indexOf('t'));
 
-const newString = gameName.substring(0,4)
-console.log(newString);
+// const newString = gameName.substring(0,4)
+// console.log(newString);
 
-const anotherString = gameName.slice(-7, 4)
-console.log(anotherString);
+// const anotherString = gameName.slice(-7, 4)
+// console.log(anotherString);
+ 
+const array1 = ['s', 'a', 'r', 't']
+const array2 = ['h', 'a', 'k']
+const array3 = array1.concat(array2)
+console.log(array3)
