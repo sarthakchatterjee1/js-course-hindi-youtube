fc53230c0a49e32fb2ea043da0546d3662136c04
SARTHAK CHATTERJEE <67676840+sarthakchatterjee1@users.noreply.github.com>
2024-02-29 23:13:40 +0000

    Objects part 2

---
+++ 02_basics/04_object.js
@@ -0,0 +1,13 @@
+//const tinderUser = new Object() //this is a singleton object 
+const tinderUser = {} // This is a non singleton object
+// In both way we can decleare object but remember the difference
+
+tinderUser.id = "123abc",
+tinderUser.name = "Sam",
+tinderUser.isLogggedIn = "false"
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com"
+}
