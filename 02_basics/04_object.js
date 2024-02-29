8ed27333e43ea3da77c81565a369b003ce932e2d
SARTHAK CHATTERJEE <67676840+sarthakchatterjee1@users.noreply.github.com>
2024-02-29 23:25:42 +0000

    Object part 2

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -9,5 +9,20 @@ tinderUser.isLogggedIn = "false"
 // console.log(tinderUser);
 
 const regularUser = {
-    email: "some@gmail.com"
+    email: "some@gmail.com",
+    fullName: {
+        userfullName:{
+            firstName: "Sarthak",
+            lastName: "Chatterjee"
+        }
+
+    }
 }
+
+//console.log(regularUser.fullName.userfullName.firstName);
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+//const obj3 = {obj1, obj2} //result is {obj1: {1:"a", 2:"b"}, obj2: {1:"c", 2:"d"}}
+const obj3 = Object.assign({}, obj1, obj2) //result is {1:"a", 2:"b", 3:"c", 4:"d"}
+console.log(obj3);
