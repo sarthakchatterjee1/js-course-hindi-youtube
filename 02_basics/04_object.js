a5b4bd0fdbe7b4940f1d87daf42a1cba61a4c638
SARTHAK CHATTERJEE <67676840+sarthakchatterjee1@users.noreply.github.com>
2024-03-01 00:18:10 +0000

    Object part2

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -24,5 +24,33 @@ const regularUser = {
 const obj1 = {1:"a", 2:"b"}
 const obj2 = {3:"c", 4:"d"}
 //const obj3 = {obj1, obj2} //result is {obj1: {1:"a", 2:"b"}, obj2: {1:"c", 2:"d"}}
-const obj3 = Object.assign({}, obj1, obj2) //result is {1:"a", 2:"b", 3:"c", 4:"d"}
+//const obj3 = Object.assign({}, obj1, obj2) //result is {1:"a", 2:"b", 3:"c", 4:"d"}
+
+const obj3 = {...obj1, ...obj2} // By using spread operator
 console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "S1@gmail.com"
+    },
+    {
+        id: 2,
+        email: "S2@gmail.com"
+    },
+    {
+        id: 3,
+        email: "S3@gmail.com"
+    }
+]
+
+console.log(users[0].email)
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser)); // In this way we can find keys in array format
+console.log(Object.values(tinderUser)); // In this way we can find valus in array format
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLogggedIn')); // It will find if any kind of property is present in the objet or not
+
